91df2c5daeccc7578df453808d8ca3c28c86c1eb
pranyajain3 <108532843+pranyajain3@users.noreply.github.com>
2023-08-08 18:22:49 +0530

    Add files via upload

---
+++ Project C110/roll-a-dice.py
@@ -0,0 +1,44 @@
+import random
+response = "y"
+
+while response == "y":
+    no = random.randint(1,6)
+
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 2:
+        print("[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[ 000 ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6:
+        print("[-----]")
+        print("[ 000 ]")
+        print("[     ]")
+        print("[ 000 ]")
+        print("[-----]")
+    response = input("Press y to roll again and n to exit:")
+    print("\n")
